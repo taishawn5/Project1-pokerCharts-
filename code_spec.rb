b10ea0630d7747bdd71f0891d06f1d8b90d75275
taishawn5 <taishawnfontleroy@gmail.com>
2022-01-06 20:27:35 -0800

    adding functionality and organization

---
+++ code_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+
+require_relative './code'
+
+RSpec.describe Position do
+  describe "pos1" do
+    it "is true for UTG and UTG+1" do
+
+    end
+  end
+end
+
+RSpec.describe Hand do
+  describe "pair?" do
+    it "is true if card values match" do
+
+    end
+  end
+  describe "suited?" do
+    it "is true if suits match" do
+
+    end
+  end
+  describe "high_value" do
+    it "returns the highest card value" do
+
+    end
+  end
+end
+
+
+RSpec.describe "calculate_action" do
+  it "is RAISE when a pos1 has a high-value pair hand" do
+
+  end
+
+
+end
