89befd850dbc1f076c384be4edf32c0b50c5ad34
taishawn5 <taishawnfontleroy@gmail.com>
2022-01-20 13:52:39 -0800

    Fixed all test cases to take in proper values and fixed all eq statements. Next is to finish Calculate tests.

--- code_spec.rb
+++ code_spec.rb
@@ -5,27 +5,27 @@ require_relative './code'
 RSpec.describe Position do
   describe "pos1" do
     it "is true for UTG and UTG+1" do
-      expect(Position.new(9, 3, 9).pos1?).to eq(3) || eq(4)
+      expect(Position.new(9, 3, 9).pos1?).to eq(true)
     end
   end
   describe "pos2" do
-    it "is true for UTG and UTG+1" do
-      expect(Position.new(9, 5, 9).pos1?).to eq(5) || eq(6)
+    it "is true for UTG+2 and LJ" do
+      expect(Position.new(9, 5, 9).pos1?).to eq(true)
     end
   end
   describe "pos3" do
-    it "is true for UTG and UTG+1" do
-      expect(Position.new(9, 7, 9).pos1?).to eq(7) || eq(8)
+    it "is true for HJ and CO" do
+      expect(Position.new(9, 7, 9).pos1?).to eq(true)
     end
   end
   describe "pos4" do
-    it "is true for UTG and UTG+1" do
-      expect(Position.new(9, 9, 9).pos1?).to eq(9)
+    it "is true for dealer buton" do
+      expect(Position.new(9, 9, 9).pos1?).to eq(true)
     end
   end
   describe "pos5" do
-    it "is true for UTG and UTG+1" do
-      expect(Position.new(9, 1, 9).pos1?).to eq(1)
+    it "is true for small blind" do
+      expect(Position.new(9, 1, 9).pos1?).to eq(true)
     end
   end
 end
@@ -33,23 +33,31 @@ end
 RSpec.describe Hand do
   describe "pair?" do
     it "is true if card values match" do
-      expect(Hand.new(card1, card2).pair?).to eq(card1.value == card2.value)
+      expect(Hand.new(Card.new(5, 'Spades'), Card.new(5, 'Hearts')).pair?).to eq(true)
     end
   end
   describe "suited?" do
     it "is true if suits match" do
-      expect(Hand.new(card1, card2).suited?).to eq(card1.suit == card2.suit)
+      expect(Hand.new(Card.new(5, 'Spades'), Card.new(10, 'Spades')).suited?).to eq(true)
     end
   end
   # how to handle each if case when writing tests
   describe "high_value" do
     it "returns the highest card value" do
+      expect(Hand.new(Card.new(5,'Spades'), Card.new(2,'Spades')).high_value).to eq(3) #suited
+      expect(Hand.new(Card.new(5,'Clubs'), Card.new(5,'Spades')).high_value).to eq(8)  #pair
+
+      expect(Hand.new(Card.new(5,'Spades'), Card.new(6,'Hearts')).high_value).to eq(2) #connectors
+      expect(Hand.new(Card.new(5,'Spades'), Card.new(4,'Hearts')).high_value).to eq(2) #connectors
+
+      expect(Hand.new(Card.new(5,'Clubs'), Card.new(7,'Spades')).high_value).to eq(1)  #gap Connectors
+      expect(Hand.new(Card.new(5,'Clubs'), Card.new(3,'Spades')).high_value).to eq(1)  #gap Connectors
 
     end
   end
   describe "value_if_suited" do
     it "is true if card values equal expected value"
-    expect(Hand.new(card1, card2).value_if_suited).to eq(card1.value + card2.value)
+    expect(Hand.new(Card.new(5,'Clubs'), Card.new(5,'Spades')).value_if_suited).to eq(10)
   end
 end
 
@@ -57,15 +65,15 @@ end
 RSpec.describe Stack do
   describe "small" do
     it "is true if BB is less than  20"
-    expect(Stack.new(current_blind, chip_stack).small?).to eq(bb_amt < 20)
+    expect(Stack.new(current_blind = 2, chip_stack = 30).small?).to eq(true)
   end
   describe "average" do
     it "is true if BB is more than 20 and less than 40"
-    expect(Stack.new(current_blind, chip_stack).average?).to eq(bb_amt > 20 && bb_amt < 40)
+    expect(Stack.new(current_blind = 2, chip_stack = 50).average?).to eq(true)
   end
   describe "large" do
     it "is true if BB is more than 40"
-    expect(Stack.new(current_blind, chip_stack).large?).to eq(bb_amt > 40)
+    expect(Stack.new(current_blind = 2, chip_stack = 100).large?).to eq(true)
   end
 end
 
@@ -73,6 +81,4 @@ RSpec.describe "calculate_action" do
   it "is RAISE when a pos1 has a high-value pair hand" do
 
   end
-
-
 end
