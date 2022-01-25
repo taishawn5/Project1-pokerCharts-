d476c887c89e53fbfd1ec0a935f077d80c00c7cf
taishawn5 <taishawnfontleroy@gmail.com>
2022-01-25 11:03:21 -0800

    Client is finished will add a few more inputs.

--- Makefile
+++ Makefile
@@ -4,6 +4,4 @@ test:
 	rspec code_spec.rb
 
 clitests:
-	ruby cli.rb --position=2
-	#ruby cli.rb --position=2 --dealer=4 --card1-value=4 --card1-suit=spades --card2-value=6 --card2-suit=spades --stack=35 --blind=2
-	#ruby cli.rb --position=2 --dealer=4 --card1-value=4 --card1-suit=spades --card2-value=6 --card2-suit=diamonds --stack=35 --blind=2
+	ruby cli.rb --position=2 --dealer=8 --card1v=14 --card1s=spades --card2v=2 --card2s=spades --stack=35 --blind=2
--- cli.rb
+++ cli.rb
@@ -7,12 +7,33 @@ OptionParser.new do |opts|
   opts.on("-pPOS", "--position=POS", "doc this") do |val|
     @options[:position] = Integer(val)
   end
+  opts.on("-dDEAL", "--dealer=DEAL", "doc this") do |val|
+    @options[:dealer] = Integer(val)
+  end
+  opts.on("-cCARD1V", "--card1v=CARD1V", "doc this") do |val|
+    @options[:card1v] = Integer(val)
+  end
+  opts.on("-cCARD1S", "--card1s=CARD1S", "doc this") do |val|
+    @options[:card1s] = String(val)
+  end
+  opts.on("-cCARD2V", "--card2v=CARD2V", "doc this") do |val|
+    @options[:card2v] = Integer(val)
+  end
+  opts.on("-cCARD2S", "--card2s=CARD2S", "doc this") do |val|
+    @options[:card2s] = String(val)
+  end
+  opts.on("-sSTACK", "--stack=STACK", "doc this") do |val|
+    @options[:stack] = Integer(val)
+  end
+  opts.on("-bBLIND", "--blind=BLIND", "doc this") do |val|
+    @options[:blind] = Integer(val)
+  end
 end.parse!
 
 action = calculate_action(
-  Position.new(9, @options[:position], 9),
-  Hand.new(Card.new(2,'Spades'), Card.new(3, 'Spades')),
-  Stack.new(current_blind = 2, chip_stack = 10)
+  Position.new(9, @options[:position], @options[:dealer]),
+  Hand.new(Card.new(@options[:card1v], @options[:card1s]), Card.new(@options[:card2v], @options[:card2s])),
+  Stack.new(@options[:blind], @options[:stack])
 )
 
 puts action
